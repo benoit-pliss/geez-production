77bf4c666efe0f8757098236419a84c75ea174f6
benoit-pliss <62262426+benoit-pliss@users.noreply.github.com>
2023-09-22 22:37:50 +0200

    adding all framworks

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
 import './bootstrap';
+
+import { createApp } from 'vue';
+import router from './router/index.js';
+
+import Home from "./components/home.vue";
+
+
+createApp({
+    components: {
+        Home,
+    }
+}).use(router).mount('#app');
---
+++ resources/js/components/home.vue
@@ -0,0 +1,67 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="content hero min-h-screen" style="background-size: cover;">
+        <div class="hero-overlay bg-opacity-60"></div>
+        <div class="hero-content text-center text-neutral-content">
+            <div class="max-w-xl">
+                <h1 class="text-5xl font-bold">GEEZ PRODUCTION</h1>
+                <p class="py-6">Nous vous souhaitons la bienvenue sur notre site web. Nous sommes malheureusement en construction, mais nous serons bientôt prêts à vous accueillir.</p>
+                <p class="pt-6">En attendant, bonne nouvelle !</p>
+                <p class="pb-6">Vous pouvez nous suivre sur les réseaux sociaux !</p>
+                <div class="flex justify-center gap-10">
+                    <a href="https://www.instagram.com/" target="_blank" class="text-4xl"><i class="fa-brands fa-instagram"></i></a>
+                    <a href="https://www.twitter.com/" target="_blank" class="text-4xl"><i class="fa-brands fa-twitter"></i></a>
+                    <a href="https://www.youtube.com/" target="_blank" class="text-4xl"><i class="fa-brands fa-youtube"></i></a>
+                    <a href="https://www.linkedin.com/" target="_blank" class="text-4xl"><i class="fa-brands fa-linkedin-in"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <footer class="footer p-10 bg-base-100 text-base-content">
+        <nav>
+            <header class="footer-title">Services</header>
+            <a class="link link-hover">Photographe</a>
+            <a class="link link-hover">Videaste</a>
+            <a class="link link-hover">Bien plus...</a>
+        </nav>
+        <nav>
+            <header class="footer-title">Geez Production</header>
+            <a class="link link-hover">À propos</a>
+            <a class="link link-hover">Contact</a>
+            <a class="link link-hover">Carrière</a>
+            <a class="link link-hover">Blog</a>
+        </nav>
+        <nav>
+            <header class="footer-title">Juridique</header>
+            <a class="link link-hover">Conditions d'utilisation</a>
+            <a class="link link-hover">Politique de confidentialité</a>
+            <a class="link link-hover">Mentions légales</a>
+            <a class="link link-hover">Politique de cookies</a>
+        </nav>
+    </footer>
+    <footer class="footer px-10 py-4 bg-base-100 text-base-content">
+        <aside class="items-center grid-flow-col gap-4">
+            <i class="fa-solid fa-clapperboard text-2xl"></i>
+            <p>© 2023 Geez Production - Tous droits réservés</p>
+        </aside>
+        <nav class="md:place-self-center md:justify-self-end">
+            <div class="grid grid-flow-col gap-4">
+                <a href="https://www.instagram.com/" target="_blank" class="text-xl"><i class="fa-brands fa-instagram"></i></a>
+                <a href="https://www.twitter.com/" target="_blank" class="text-xl"><i class="fa-brands fa-twitter"></i></a>
+                <a href="https://www.youtube.com/" target="_blank" class="text-xl"><i class="fa-brands fa-youtube"></i></a>
+                <a href="https://www.linkedin.com/" target="_blank" class="text-xl"><i class="fa-brands fa-linkedin-in"></i></a>
+            </div>
+        </nav>
+    </footer>
+</template>
+
+<style scoped>
+
+.content {
+    background-image: url('../../img/bg-neon.jpg');
+}
+
+</style>
