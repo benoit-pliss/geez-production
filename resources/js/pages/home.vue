b00f2700c1521e613b93ab05f51fb101148449d7
benoit-pliss <62262426+benoit-pliss@users.noreply.github.com>
2024-02-26 00:08:49 +0100

    build to deploy

--- resources/js/pages/home.vue
+++ resources/js/pages/home.vue
@@ -15,24 +15,24 @@ const hovered = ref('')
         <div class="flex h-screen flex-col md:flex-row">
             <!-- Curtain 1 - Initially 50% and Increases to 70% on Hover -->
             <a class="relative w-full md:w-1/2 h-full hover:z-10 transition-all duration-500"
-                @mouseover="hovered = 'left'" 
+                @mouseover="hovered = 'left'"
                 @mouseleave="hovered = ''"
                 :class="{ 'md:w-2/3 z-10': hovered === 'left', 'md:w-1/3 z-10': hovered === 'right'}"
                 href="/photo">
-                <img class="absolute inset-0 h-full w-full object-cover" src="/storage/images/IMG_6480.jpg" alt="Image">
+                <img class="absolute inset-0 h-full w-full object-cover" src="../../img/acceuil/IMG_6480.jpg" alt="Image">
                 <div class="absolute inset-0 flex items-center justify-center flex-col bg-black bg-opacity-50">
                     <h1 class="text-4xl font-bold text-white">Photographie</h1>
                 </div>
             </a>
-            
+
             <!-- Curtain 2 - Initially 50% and Decreases to 30% on Hover of the other -->
             <a class="relative w-full md:w-1/2 h-full hover:z-10 transition-all duration-500"
-                @mouseover="hovered = 'right'" 
+                @mouseover="hovered = 'right'"
                 @mouseleave="hovered = ''"
                 :class="{ 'md:w-1/3 z-10': hovered === 'left', ' md:w-2/3 z-10': hovered === 'right'}"
                 href="/video">
                 <video class="absolute inset-0 h-full w-full object-cover" autoplay loop muted>
-                    <source src="/storage/videos/ALCHEMIA%20LOUNGE%20CUISINE/BCDD794F-665D-4FBD-B58D-C457D492280C.MOV" type="video/mp4">
+                    <source src="../../img/acceuil/videos/EV1.mp4" type="video/mp4">
                 </video>
                 <div class="absolute inset-0 h-full flex items-center justify-center flex-col bg-black bg-opacity-50">
                     <h1 class="text-4xl font-bold text-white">Audiovisuel</h1>
