e2f4e2582a468a748bf4f8d321895c0ce020ad4d
benoit-pliss <62262426+benoit-pliss@users.noreply.github.com>
2024-03-13 20:03:47 +0100

    c'est moche mais ya un favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,8 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
+        <link rel="icon" href="{{ asset('favicon-geez.ico') }}">
+
         <title>Geez-Production</title>
 
         @vite(['resources/css/app.css', 'resources/js/app.js'])
