4d0a256e882f225afb99c160c6e8667ec070dcc1
benoit-pliss <62262426+benoit-pliss@users.noreply.github.com>
2024-03-25 14:47:29 +0100

    carousel avec vue3-carousel

--- resources/js/components/videos/carousel-videos.vue
+++ resources/js/components/videos/carousel-videos.vue
@@ -1,187 +1,131 @@
-<script>
-// Import Swiper core and required modules
-import { Navigation, Pagination, Scrollbar, A11y, EffectCoverflow } from 'swiper/modules';
-
-// Import Swiper Vue.js components
-import {Swiper, SwiperSlide} from 'swiper/vue';
-
-// Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
-
-import 'swiper/css/effect-coverflow';
-import 'swiper/css/pagination';
-
-import {onMounted, ref} from "vue";
-
-// Import Swiper styles
-export default {
-    components: {
-        Swiper,
-        SwiperSlide,
-    },
-    methods: {
-        updateSwiperHeight() {
-            this.mySwiper.updateAutoHeight();
-        }
-    },
-    setup() {
-        const mySwiper = ref(null);
-        const videos = ref([]);
-
-        const onSwiper = (swiper) => {
-            mySwiper.value = swiper;
-        };
-
-        const onSlideChange = () => {
-            const swiper = mySwiper.value;
-            if (swiper.activeIndex < videos.value.length) {
-                const activeSlideVideo = videos.value[swiper.activeIndex];
-                activeSlideVideo.play();
-
-                videos.value.forEach((video, index) => {
-                    if (index !== swiper.activeIndex) {
-                        video.pause();
-                        video.currentTime = 0; // Réinitialise la position de lecture de la vidéo
-                    }
-                });
-            }
-        };
-        const reachEnd = () => {
-            // reset the swiper
-            mySwiper.value.slideTo(0);
-
-            // Réinitialiser et relancer la première vidéo
-            videos.value.forEach((video, index) => {
-                video.pause();
-                video.currentTime = 0;
-                if (index === 0) {
-                    video.play();
-                }
-            });
-        };
-
-        onMounted(() => {
-            //lancer la première vidéo
-            videos.value[0].play();
-
-            // Précharger les vidéos
-            videos.value.forEach((video) => {
-                const videoElement = document.createElement('video');
-                videoElement.src = video.url;
-                videoElement.preload = 'auto';
-            });
-        });
-
-        return {
-            mySwiper,
-            videos,
-            onSwiper,
-            onSlideChange,
-            reachEnd,
-            modules: [Navigation, Pagination, Scrollbar, A11y, EffectCoverflow]
-        };
-    },
-
-    data() {
-        return {
-            video : ref(),
-            videos : [
-                {
-                    id: 1,
-                    url: 'https://geez-production.com/storage/videos/EV2.mp4'
-                },
-                {
-                    id: 2,
-                    url: 'https://geez-production.com/storage/videos/EV1.mp4'
-                },
-                {
-                    id: 3,
-                    url: 'https://geez-production.com/storage/videos/EV4.mp4'
-                },
-                {
-                    id: 4,
-                    url: 'https://geez-production.com/storage/videos/EV2.mp4'
-                },
-                {
-                    id: 5,
-                    url: 'https://geez-production.com/storage/videos/EV1.mp4'
-                },
-                {
-                    id: 6,
-                    url: 'https://geez-production.com/storage/videos/EV4.mp4'
-                }
-            ]
-        };
-    },
-};
-
-
-
-</script>
-
-
-
-
-
 <template>
-    <div class="flex items-center justify-center h-screen bg-gray-800 text-white font-bold text-2xl">
-        <swiper
-            ref="mySwiper"
-            :modules="modules"
-            :slides-per-view="3"
-            :space-between="50"
-            navigation
-            pagination
-            :scrollbar="{ draggable: false, hide : true , el: '.swiper-scrollbar' }"
-            @swiper="onSwiper"
-            @slideChange="onSlideChange"
-            @reachEnd="reachEnd"
-            centeredSlide
-            :effect="'coverflow'"
-            :grabCursor="true"
-            :centeredSlides="true"
-            :coverflowEffect="{
-            rotate: 50,
-            stretch: 30,
-            depth: 200,
-            modifier: 1,
-            slideShadows: true
-            }"
-            :autoHeight="true"
-
-        >
-            <swiper-slide v-for="video in videos" :key="video.id">
-                <video ref="videos" class="video-slide" loop muted v-bind="video" preload="auto" controls @loadedmetadata="updateSwiperHeight">
-                    <source :src="video.url" type="video/mp4">
-                    <track kind="captions" :src="video.captions" srclang="fr" label="French captions">
-                </video>
-            </swiper-slide>
-        </swiper>
-    </div>
+    <carousel
+        ref="myCarousel"
+        :items-to-show="3.95"
+        :wrap-around="true"
+        :transition="500"
+        @slide-end="onSlideChange"
+    >
+        <slide v-for="(video, index) in videos" :key="video.id">
+            <video :src="video.url" preload="auto" @loadedmetadata="addVideo($event, index)" muted loop controls>
+            </video>
+        </slide>
+        <template #addons>
+            <Navigation />
+        </template>
+    </carousel>
 </template>
 
+<script setup>
+import 'vue3-carousel/dist/carousel.css'
+import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel'
+import {nextTick, onMounted, ref, watch} from "vue";
+
+const videoElements = ref([]);
+const myCarousel = ref(null);
+const videos = ref([
+    {
+        id: 1,
+        url: 'https://geez-production.com/storage/videos/EV2.mp4',
+    },
+    {
+        id: 2,
+        url: 'https://geez-production.com/storage/videos/EV1.mp4',
+    },
+    {
+        id: 3,
+        url: 'https://geez-production.com/storage/videos/EV4.mp4',
+    },
+    {
+        id: 4,
+        url: 'https://geez-production.com/storage/videos/EV2.mp4',
+    },
+    {
+        id: 5,
+        url: 'https://geez-production.com/storage/videos/EV1.mp4',
+    },
+    {
+        id: 6,
+        url: 'https://geez-production.com/storage/videos/EV4.mp4',
+    },
+]);
+
+
+const currentIndexes = ref({
+    currentSlideIndex : 0,
+    prevSlideIndex : 0,
+    slidesCount : videos.value.length,
+    slidingToIndex : null,
+});
+
+const addVideo = (event, index) => {
+    const videoElement = event.target;
+    videoElement.addEventListener('canplay', () => {
+        if (index === currentIndexes.value.currentSlideIndex && videoElement.paused) {
+            videoElement.play();
+        }
+    });
+    videoElements.value[index] = videoElement;
+};
+
+const onSlideChange = (event) => {
+    currentIndexes.value = event;
+    videoElements.value.forEach((videoElement, index) => {
+        if (videoElement.readyState >= 3) {
+            if (index === currentIndexes.value.currentSlideIndex) {
+                videoElement.play();
+            } else {
+                videoElement.pause();
+                videoElement.currentTime = 0;
+            }
+        }
+    });
+};
+</script>
+
 <style scoped>
-.swiper {
-    width: 100%;
-    height: auto;
+.carousel__slide {
+    padding: 5px;
 }
 
-.swiper-slide {
-    text-align: center;
-    font-size: 18px;
-    background: #fff;
+.carousel__viewport {
+    perspective: 2000px;
 }
 
-.swiper .swiper-slide {
-    height: 300px;
-    line-height: 300px;
+.carousel__track {
+    transform-style: preserve-3d;
 }
 
-.swiper .swiper-slide:nth-child(2n) {
-    height: auto;
-    line-height: 500px;
+.carousel__slide--sliding {
+    transition: 0.5s;
+}
+
+.carousel__slide {
+    opacity: 0.9;
+    transform: rotateY(-20deg) scale(0.9);
+}
+
+.carousel__slide--active ~ .carousel__slide {
+    transform: rotateY(20deg) scale(0.9);
+}
+
+.carousel__slide--prev {
+    opacity: 1;
+    transform: rotateY(-10deg) scale(0.95);
+}
+
+.carousel__slide--next {
+    opacity: 1;
+    transform: rotateY(10deg) scale(0.95);
+}
+
+.carousel__slide--active {
+    opacity: 1;
+    transform: rotateY(0) scale(1.1);
+}
+
+.navigation-container {
+    display: flex;
+    justify-content: space-between; /* Optional: To add space between the buttons */
 }
 </style>
