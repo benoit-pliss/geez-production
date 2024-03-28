92f9199aa62362973ef28eb06ad2aeacddb9c8c6
benoit-pliss <62262426+benoit-pliss@users.noreply.github.com>
2024-03-28 15:35:16 +0100

    DB is good now

---
+++ database/migrations/2024_03_28_142808_create_files_type_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('files_type', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->timestamps();
+        });
+
+        Schema::table('files', function (Blueprint $table) {
+            $table->renameColumn('type', 'id_type');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('files_type');
+
+        Schema::table('files', function (Blueprint $table) {
+            $table->renameColumn('id_type', 'type');
+        });
+    }
+};
