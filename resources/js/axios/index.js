8c137c51ba419a983c4da8928fdb802ed769b965
benoit-pliss <62262426+benoit-pliss@users.noreply.github.com>
2024-04-30 23:40:00 +0200

    change header allows
    Signed-off-by: benoit-pliss <62262426+benoit-pliss@users.noreply.github.com>

--- resources/js/axios/index.js
+++ resources/js/axios/index.js
@@ -5,6 +5,8 @@ const axiosClient = axios.create({
 })
 
 axiosClient.interceptors.request.use(config => {
+    config.headers.Accept = "application/json";
+    config.headers.Allow = "GET, POST, PUT, DELETE";
     config.headers.Authorization = `Bearer ` + localStorage.getItem("token");
     return config;
 });
