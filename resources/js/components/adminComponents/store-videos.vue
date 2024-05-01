440ad9fb24acfcddc893169f69e5e0aa89e24d39
benoit-pliss <62262426+benoit-pliss@users.noreply.github.com>
2024-05-01 20:57:01 +0200

    plusieurs files
    Signed-off-by: benoit-pliss <62262426+benoit-pliss@users.noreply.github.com>

--- resources/js/components/adminComponents/store-videos.vue
+++ resources/js/components/adminComponents/store-videos.vue
@@ -8,40 +8,42 @@ import notificationService from "../../services/notificationService.js";
 window.createUpload = createUpload;
 
 let uploader = null;
-const file = ref(null);
+const files = ref(null);
 const progress = 0;
 
 const submit = (e) => {
-    file.value = e.target.files[0];
+    files.value = e.target.files;
 
-    if(!file.value) {
+    if(!files.value.length) {
         return;
     }
 
-    uploader = createUpload({
-        file: file.value,
-        endpoint: '/api/upload/chunks',
-        headers: {
-            'Content-Type': 'application/json',
-            'Bearer': 'Bearer ' + localStorage.getItem('token')
-        },
-        method: 'POST',
-        chunkSize: 10 * 1024,
-    });
+    Array.from(files.value).forEach(file => {
+        uploader = createUpload({
+            file: file,
+            endpoint: '/api/upload/chunks',
+            headers: {
+                'Content-Type': 'application/json',
+                'Bearer': 'Bearer ' + localStorage.getItem('token')
+            },
+            method: 'POST',
+            chunkSize: 10 * 1024,
+        });
 
-    uploader.on('chunkSuccess', (response) => {
-        if (!response.detail.response.body) {
-            return;
-        }
+        uploader.on('chunkSuccess', (response) => {
+            if (!response.detail.response.body) {
+                return;
+            }
 
-        handleSuccess(file.value.name, JSON.parse(response.detail.response.body).file)
-            .then((res) => {
-                if (res.data.success){
-                    notificationService.addToast('Vidéo enregistrée avec succès', 'success');
-                } else {
-                    notificationService.addToast('Erreur lors de l\'enregistrement de la vidéo', 'error');
-                }
-            })
+            handleSuccess(file.name, JSON.parse(response.detail.response.body).file)
+                .then((res) => {
+                    if (res.data.success){
+                        notificationService.addToast('Vidéo enregistrée avec succès', 'success');
+                    } else {
+                        notificationService.addToast('Erreur lors de l\'enregistrement de la vidéo', 'error');
+                    }
+                })
+        });
     });
 };
 
@@ -56,25 +58,17 @@ const submit = (e) => {
                 <div class="mt-4 flex text-sm leading-6 text-gray-600">
                     <label for="file-upload" class="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500">
                         <span>Upload a file</span>
-                        <input id="file-upload" name="file-upload" v-on:change.prevent="submit" type="file" class="hidden"/>
+                        <input id="file-upload" name="file-upload" v-on:change.prevent="submit" type="file" class="hidden" multiple/>
                     </label>
                     <p class="pl-1">or drag and drop</p>
                 </div>
-                <p v-if="file"
+                <p v-if="files"
                    class="mt-2 text-sm text-gray-500"
                 >
-                    {{ file.name }}
+                    {{ files.length }} files selected
                 </p>
             </div>
         </div>
-<!--        <div class="mt-6 flex justify-center items-center">-->
-<!--            <button v-if="!isUploading" type="button" @click="upload"-->
-<!--                    class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus-visible:outline focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-indigo-500">-->
-<!--                Valider-->
-<!--            </button>-->
-
-<!--            <progress v-if="isUploading" class="progress w-56"></progress>-->
-<!--        </div>-->
     </div>
 </template>
 
