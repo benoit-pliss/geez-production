bce363e33e3c9ae8ed4196101725118e494e5723
victorfqt <mediaandco@victorfouquet.com>
2024-07-31 11:21:46 +0200

    Test update

--- resources/js/components/GalerieVideo.vue
+++ resources/js/components/GalerieVideo.vue
@@ -30,39 +30,7 @@
                     </div>
                 </Combobox>
             </div>
-
-            <!-- Last gallery -->
-            <!--
-            <div class="mx-auto mt-16 flow-root max-w-2xl sm:mt-20 lg:mx-0 lg:max-w-none">
-                <div class="-mt-8 sm:-mx-4 sm:columns-2 sm:text-[0] sm:columns-2 md:columns-2 lg:columns-3 xl:columns-4">
-                    <div v-for="video in videos" :key="video.name" class="pt-8 sm:inline-block sm:w-full sm:px-4">
-                        <div class="overflow-hidden transition duration-300 transform rounded-lg">
-                            <video :src="video.url" :poster="video.poster_url" :ref="el => { videoPlayers[video.id] = el; }" preload="none" class="object-cover w-full h-auto" :muted="false" :controls="false" v-on:mouseover="playVideo(video)"
-                                loop data-id="video.id"></video>
-                            <div class="absolute bottom-0 flex place-content-end justify-start flex-wrap-reverse gap-2 p-4 h-fit" v-if="!video.showControls">
-                                <div class="flex gap-x-2 text-white backdrop-blur-md bg-black/10 rounded-lg items-center justify-center px-2 py-1">
-                                    <div>
-                                        <PlayIcon class="h-4 w-4"
-                                            v-if="!video.play"
-                                            v-on:click="playVideo(video)"/>
-                                        <PauseIcon class="h-4 w-4"
-                                            v-if="video.play"
-                                            v-on:click="pauseVideo(video)"/>
-                                    </div>
-                                    <div>
-                                        <ArrowPathIcon class="h-4 w-4"
-                                            v-on:click="goStart(video)"/>
-
-                                    </div>
-                                </div>
-                                <Badge v-for="tag in video.tags" :key="tag.id" :label="tag.name" :color="tag.color" type="add" v-on:click="addTag(tag)" :id="tag.id"/>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            -->
-
+            
             <!-- New gallery -->
             <!-- New video gallery -->
             <div class="mx-auto mt-16 flow-root max-w-2xl sm:mt-20 md:mx-0 md:max-w-none px-4">
